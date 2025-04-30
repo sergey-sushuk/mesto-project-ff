ef7678154ed758de5d702b744d86f25e8fc33574
сергей сущук <serega-sushuk@yandex.ru>
2025-04-30 21:45:37 +0300

    Update index.js

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,33 @@
 // @todo: Темплейт карточки
-
-// @todo: DOM узлы
+const placesList = document.querySelector(".places__list");
 
 // @todo: Функция создания карточки
+function createCard(card, deleteCard) {
+  const cardTemplate = document.querySelector("#card-template").content;
+  const cardElement = cardTemplate.cloneNode(true).firstElementChild;
+
+  cardElement.querySelector(".card__title").textContent = card.name;
+  cardElement.querySelector(".card__image").src = card.link;
+  const resetButton = cardElement.querySelector(".card__delete-button");
+
+  resetButton.addEventListener("click", () => {
+    deleteCard(cardElement);
+  });
+
+  return cardElement;
+}
 
 // @todo: Функция удаления карточки
+function deleteCard(cardElement) {
+  cardElement.remove();
+}
 
 // @todo: Вывести карточки на страницу
+function addCard(cards, container, deleting) {
+  cards.forEach((card) => {
+    const cardElement = createCard(card, deleting);
+    container.append(cardElement);
+  });
+}
+
+addCard(initialCards, placesList, deleteCard);
