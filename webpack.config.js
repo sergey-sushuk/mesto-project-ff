69a430549762324994e44cdc9a7177b7d61d1fe5
sergey sushuk <serega-sushuk@yandex.ru>
2025-06-08 16:07:10 +0300

    23

--- webpack.config.js
+++ webpack.config.js
@@ -1,64 +1,57 @@
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
 module.exports = {
-  entry: { main: './src/index.js' },
+  entry: {
+    main: './src/index.js'
+  },
   output: {
-    filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
-    publicPath: '/',
+    filename: 'main.js',
+    publicPath: '',
   },
   mode: 'development',
   devServer: {
     static: path.resolve(__dirname, './dist'),
-    compress: true,
-    port: 8080,
     open: true,
+    compress: true,
+    port: 8080
   },
   module: {
-    rules: [
-      {
-        test: /\.css$/i,
-        use: [
-          MiniCssExtractPlugin.loader,
-          'css-loader'
-        ],
+    rules: [{
+        test: /\.js$/,
+        use: 'babel-loader',
+        exclude: '/node_modules/'
       },
-      {
+        {
         test: /\.html$/i,
         loader: 'html-loader'
       },
       {
-                test: /\.(png|svg|jpg|jpeg|gif)$/i,
-                type: 'asset/resource',
-                generator: {
-                    filename: 'images/[name].[hash][ext]'
-                }
-            
+        test: /\.(png|svg|jpg|gif|woff(2)?|eot|ttf|otf)$/,
+        type: 'asset/resource'
+      },
+      {
+        test: /\.css$/,
+        use: [MiniCssExtractPlugin.loader, {
+            loader: 'css-loader',
+            options: {
+              importLoaders: 1
+            }
           },
-     {
-  test: /\.js$/, // Обрабатываем все JS файлы
-  exclude: /node_modules/, // исключая node_modules
-  use: {
-    loader: 'babel-loader',
-  },
-}
-    ],
-  },
-
-  resolve: {
-        modules: [
-            path.resolve(__dirname, 'src'),
-            path.resolve(__dirname, 'images'),
-            'node_modules'
+          'postcss-loader'
         ]
-    },
+      },
+    ]
+  },
   plugins: [
-    new MiniCssExtractPlugin({
-      filename: 'styles.css',
-    }),
     new HtmlWebpackPlugin({
-      template: './src/index.html',
+      template: './src/index.html'
     }),
-  ],
-};
+    new CleanWebpackPlugin(),
+    new MiniCssExtractPlugin(),
+
+  ]
+}
